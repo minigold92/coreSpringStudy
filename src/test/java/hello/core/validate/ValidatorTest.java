f78d38aa32e56c2e9fbc649cb2e6804907d3ac7c
mgh23 <minigold92@gmail.com>
2022-06-19 23:09:29 +0900

    마스킹 테스트케이스 추가

--- src/test/java/hello/core/validate/ValidatorTest.java
+++ src/test/java/hello/core/validate/ValidatorTest.java
@@ -1,6 +1,7 @@
 package hello.core.validate;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +17,23 @@ class ValidatorTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    void otherTypeResidentNumber() {
+        String inputNumber="920530-1111111";
+        String expectedNumber = "920530-1******";
+        try {
+            Assertions.assertEquals(Validator.maskResidentNumber(inputNumber), expectedNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("비정상적인 주민등록번호")
+    void invalidResidentNumber(){
+        String inputNumber="9205301111";
+        Assertions.assertThrows(Exception.class, ()->Validator.maskResidentNumber(inputNumber));
     }
 }
