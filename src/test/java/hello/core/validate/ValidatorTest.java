6dcbf4a0c6b9e8c58eecfd7a6173b178adc38759
mgh23 <minigold92@gmail.com>
2022-06-20 00:07:52 +0900

    test Exception 변경

--- src/test/java/hello/core/validate/ValidatorTest.java
+++ src/test/java/hello/core/validate/ValidatorTest.java
@@ -9,31 +9,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidatorTest {
 
     @Test
-    void maskResidentNumber() {
-        String inputNumber="9205301111111";
+    void maskResidentNumber() throws Exception {
+        String inputNumber = "9205301111111";
         String expectedNumber = "920530-1******";
-        try {
-            Assertions.assertEquals(Validator.maskResidentNumber(inputNumber), expectedNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Assertions.assertEquals(Validator.maskResidentNumber(inputNumber), expectedNumber);
     }
 
     @Test
-    void otherTypeResidentNumber() {
-        String inputNumber="920530-1111111";
+    void otherTypeResidentNumber() throws Exception {
+        String inputNumber = "920530-1111111";
         String expectedNumber = "920530-1******";
-        try {
-            Assertions.assertEquals(Validator.maskResidentNumber(inputNumber), expectedNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Assertions.assertEquals(Validator.maskResidentNumber(inputNumber), expectedNumber);
     }
 
     @Test
     @DisplayName("비정상적인 주민등록번호")
-    void invalidResidentNumber(){
-        String inputNumber="9205301111";
-        Assertions.assertThrows(Exception.class, ()->Validator.maskResidentNumber(inputNumber));
+    void invalidResidentNumber() {
+        String inputNumber = "9205301111";
+        Assertions.assertThrows(Exception.class, () -> Validator.maskResidentNumber(inputNumber));
     }
 }
