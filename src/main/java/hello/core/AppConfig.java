be8cbde2930616a17b9f36698496c00c30c57130
mgh23 <minigold92@gmail.com>
2022-06-19 19:24:08 +0900

    정리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,20 +10,25 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration//설정 정보라는 것을 나타낼 떄 씀
 public class AppConfig {
+//    각메서드에 Bean이라고 적어주면 됨.
+    @Bean//그러면 스프링 컨테이너라는 곳에 등록이 됨.
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {//ctrl + alt + M 메서드 추출
+    @Bean
+    public MemberRepository memberRepository() {//ctrl + alt + M 메서드 추출
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        //이게 스프링컨테이너라고 보면 됨.
+//        모든 객체들을 관리해줌.parameter를 넣어줘야함.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //기본적으로 받아올 때 메서드 이름으로 받아옴.두번쨰는 반환받을 클래스 타입.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);//ctrl alt v
         memberService.join(member);
